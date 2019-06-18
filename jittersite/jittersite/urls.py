3c9f099e4a860774b88877f69c571b9260c463cc
saar <saar@codehs.com>
2019-06-18 15:34:53 -0700

    started implementing built-in django user auth

--- jittersite/jittersite/urls.py
+++ jittersite/jittersite/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^jitterbunk/', include('jitterbunkapp.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
 ]

ce9a6365050bd474194545e1005f0d46c32fc5a4
saar <saar@codehs.com>
2019-06-19 12:59:52 -0700

    added logout functionality

--- jittersite/accounts/urls.py
+++ jittersite/accounts/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     url(r'^signup/$', views.SignUpView.as_view(), name='signup'),
+    url(r'^logout/$', views.logout_user, name='logout'),
 ]
--- jittersite/accounts/views.py
+++ jittersite/accounts/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse_lazy
 from django.views import generic
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
